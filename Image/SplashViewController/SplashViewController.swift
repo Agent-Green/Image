79297524b02d282cf2ee2996ea6837c15ef80c15
Agent-Green <a.2000.g1@yandex.ru>
2024-09-18 13:49:01 +0300

    Добавлена авторитизация и сплашскрин

---
+++ Image/SplashViewController/SplashViewController.swift
@@ -0,0 +1,77 @@
+//
+//  SplashViewController.swift
+//  Image
+//
+//  Created by Алиса  Грищенкова on 18.09.2024.
+//
+
+import Foundation
+import UIKit
+
+final class SplashViewController : UIViewController{
+    
+    //MARK: - Private Lets
+    private let oAuth2Storage = OAuth2TokenStorage()
+    private let oAuth2Service = OAuth2Service.shared
+    private let showAuthenticationSegueIdentifier = "ShowAuth"
+    
+    //MARK: - LifeCycle
+    override func viewDidAppear(_ animated: Bool){
+        super.viewDidAppear(animated)
+        
+        if oAuth2Storage.token != nil {
+            switchToTabBarController()
+        } else {
+            performSegue(withIdentifier: showAuthenticationSegueIdentifier, sender: nil)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    //MARK: - Private Methods
+    private func switchToTabBarController() {
+        guard let window = UIApplication.shared.windows.first else { fatalError("Invalid Configuration") }
+        let tabBarController = UIStoryboard(name: "Main", bundle: .main)
+            .instantiateViewController(withIdentifier: "TabBarViewController")
+        window.rootViewController = tabBarController
+    }
+}
+
+extension SplashViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showAuthenticationSegueIdentifier {
+            guard
+                let navigationController = segue.destination as? UINavigationController,
+                let viewController = navigationController.viewControllers[0] as? AuthViewController
+            else { fatalError("Failed to prepare for \(showAuthenticationSegueIdentifier)") }
+            viewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+}
+
+extension SplashViewController: AuthViewControllerDelegate {
+    func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
+        dismiss(animated: true) { [weak self] in
+            guard let self = self else { return }
+            self.fetchOAuthToken(code)
+        }
+    }
+    
+    private func fetchOAuthToken(_ code: String) {
+        oAuth2Service.fetchOAuthToken(code: code) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success:
+                self.switchToTabBarController()
+            case .failure:
+                // TODO [Sprint 11]
+                break
+            }
+        }
+    }
+}
